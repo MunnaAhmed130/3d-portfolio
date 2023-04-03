aec44699a965d738ed82b77c6f274d3da9cd39b4
Munna Ahmed <munnaahmed2025@gmail.com>
2023-04-04 03:02:19 +0600

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,6 +23,24 @@ const Hero = () => {
         </div>
       </div>
       <ComputersCanvas />
+
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[30px] h-[60px] rounded-3xl border-4 border-secondary flex justify-center items-start pt-2">
+            <motion.dev
+              animate={{
+                y: [0, 25, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 bg-secondary rounded-full  "
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
