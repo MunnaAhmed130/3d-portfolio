5cd0cbc921b3da2b9ce79e58ea046a6ae73b2e38
Munna Ahmed <munnaahmed2025@gmail.com>
2023-04-03 19:48:52 +0600

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>
+          <h1 className={`${styles.heroHeadText} text-white leading-tight`}>
             Hi, I'm <span className="text-[#915eff]">Munna </span>
           </h1>
           <p className={`${styles.heroSubText} m-2 text-white-100`}>
