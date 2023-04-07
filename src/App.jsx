ee0afe52d615181f9ff694f6352e009674378133
Munna Ahmed <munnaahmed2025@gmail.com>
2023-04-08 00:34:09 +0600

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,8 @@ import {
 const App = () => {
   return (
     <BrowserRouter>
-      <div>
-        <div>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
         </div>
