189de0e090261a3df9710f89ce79987050df2246
Munna Ahmed <munnaahmed2025@gmail.com>
2023-04-08 00:20:39 +0600

    commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -18,8 +18,46 @@ const Contact = () => {
     message: "",
   });
   const [loading, setLoading] = useState(false);
-  const handleChange = (e) => {};
-  const handleSubmit = (e) => {};
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+  console.log(form);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        "service_l4jjwct",
+        "template_jbjqq1s",
+        {
+          from_name: form.name,
+          to_name: "Munna",
+          from_email: form.email,
+          to_email: "munnaahmed2025@gmail.com",
+          message: form.message,
+        },
+        "user_TQOZfpaOEzhRwWYzHRoeb"
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you. I will get back to you as soon as possible");
+
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.log(error);
+          alert("something went wrong");
+        }
+      );
+  };
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
